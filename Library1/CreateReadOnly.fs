0a689e8f4805d25e22efa4dbe3315d9f6d132115
Jon Newton <jon.newton@gmail.com>
2013-11-23 20:39:35 -0700

    Toying with ops.  valueNow function is probably useful later

--- Library1/CreateReadOnly.fs
+++ Library1/CreateReadOnly.fs
@@ -44,6 +44,7 @@ type Attribute = {name : string; history : List<Fact>}          // ??? Exposing 
                                                                 // or some monad that keeps recent in memory and old on slow storage?
                                                                 // Will deal with best practices for encapsultion during second pass.
 
+// This can wait.  Get create and queries done before we support schema changes
 type Operation =
     | Deleted of string // To remove an attribute from an entity we set its value to deleted at a time
     | Renamed of string * string // value is the attribute's old name and the new name
@@ -52,8 +53,9 @@ type Operation =
 // The history of an entity as defined by its various characteristics
 type Entity = {
     id : int; 
-    attributes : Map<string, Attribute>; // The current attributes
-    ops : List<int * Operation>
+//    attributes : Map<string, Attribute> // The current attributes
+    attributes : Map<string, List<Fact>>
+//    ;ops : List<int * Operation>
 }   
 
 // All the entities
@@ -84,30 +86,41 @@ type Database = {
 // create a DB
 let createDatabase name = {name = name; entities = Map.empty; entitySeq = 0; timeSeq = 0}
 
+let createEntity id time attribMap =
+    let attribs = Map.map (fun k v -> [{value = v; time = time}]) attribMap
+    {id = id; attributes = attribs}
+    
+let transact db ops =
+    let time = db.timeSeq + 1
+    {name = db.name; entities = db.entities; entitySeq = db.entitySeq; timeSeq = time};
+
 // create transaction, add entity, create attribute, change attribute value, rename attribute, delete attribute, delete entity (perhaps special attribute?)
 
-// create an entity
-let createEntity attributes database =
-    {id = transactor.createEntityId ;   
-    attributes = Map.ofList []}                     
+// create an entity.  return the entity and the new version of the DB
+//let createEntity entityId time attribMap  =
+//    let createAttribute (name:string, value:Value) =
+//        {name; [{value; time}]}
+//    let attributes = Map.map (fun (k, v) -> createAttribute k v) attribMap
+//    {entityId; attributes}
+
+
 
 
 // record facts about an entity
-let recordFact entity attributeName attributeValue
+//let recordFact entity attributeName attributeValue
 
 //let recordFact name value entity =
     
 //let maptest = Map.ofList [(5, "foo"); (6, true); (7, 6.7)]
 
 
-let createAttribute name fact =
-    {name = name; history = fact :: []}
+//let createAttribute name fact = {name = name; history = [fact];}
 
-let readNow predicate transform = // apply transform to all entitie where predicate is true
-
-let readWhen predicate transform time = // list of entities
-
-let 
+//let readNow predicate transform = // apply transform to all entitie where predicate is true
+//
+//let readWhen predicate transform time = // list of entities
+//
+//let 
 
 // Example:
 // Create db
@@ -116,3 +129,43 @@ let
 // Create platform
 // Assign type
 // Update state every frame
+
+let db = createDatabase "test"
+let alpha = ["name", String("alpha"); "x", Float(1.0); "y", Float(8.0); "active", Boolean(true);]
+let bravo = ["name", String("bravo"); "x", Float(4.0); "y", Float(3.0); "active", Boolean(true);]
+// create entities alpha and bravo in the database
+// deactivate alpha
+// move bravo
+// select all active now
+// select all active before
+
+let valueNow attributeName entity=
+    match Map.tryFind attributeName entity.attributes with
+    | Some (fact :: _) -> fact.value
+    | Some [] -> Nothing  // this should never happen period
+    | None -> Nothing // this should never happen for time "now"
+
+// each e where "name" equals "alpha" and "active" equals true
+
+let EQUALS attribute (value : Value) =
+    (fun entity -> 
+        match entity |> valueNow attribute with
+        | Nothing -> false
+        | v -> v = value)
+
+let AND pred1 pred2 = // and
+    fun entity -> pred1 entity && pred2 entity
+
+let OR pred1 pred2 = // or
+    fun entity -> pred1 entity || pred2 entity
+
+//let v = String("alpha")
+//let nameAlpha = EQUALS("name" String("alpha"))
+
+//let pred = (AND (EQUALS ("name" String("alpha"))) (EQUALS "active" Boolean(true)))
+
+let gt5 x = x > 5
+let lt10 x = x < 10
+let foo = AND gt5 lt10
+let seven = foo 7
+let twelve = foo 12
